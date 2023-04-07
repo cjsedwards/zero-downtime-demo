c2eec7e9063d2bf45853f21f3814c0d961e974e9
Chris Edwards <chris@fillipfleet.com>
2023-04-07 16:39:28 -0600

    Stop console logging db connection string

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectionString = process.env.DB_CONNECTION_STRING ?? 'mongodb://localhost:27017/zero-downtime-demo'
-console.log(connectionString)
+
 const mongooseOptions = {
     autoIndex: true,
 }
