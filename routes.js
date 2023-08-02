5a33b4532496c6adddecda8d64c7f946785836a4
Chris Edwards <chris@fillipfleet.com>
2023-08-02 08:13:33 -0600

    Debug

--- routes.js
+++ routes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post("/increment-counter", async (req, res) => {
     try{
+        console.log(req.headers.host)
         const counter = await CounterRepo.find()
         counter.increment()
         await CounterRepo.save(counter)
